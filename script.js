9f922db9ec4400646e328b2d480ea816b48cc326
rupesh989 <rupeshbavaskar24@gmail.com>
2024-02-25 19:57:14 +0530

    added js function initials

--- script.js
+++ script.js
@@ -1,1 +1,15 @@
+const qrText = document.getElementById("qr-text");
+const sizes = document.getElementById("sizes");
+const generatBtn = document.getElementById("generateBtn");
+const downloadBtn = document.getElementById("downloadBtn");
 
+const qrContainer = document.querySelector(".qr-body");
+
+generatBtn.addEventListener("click", (e) => {
+  e.preventDefault();
+  generatQRCode();
+});
+
+function generatQRCode() {
+  new QRCode();
+}
